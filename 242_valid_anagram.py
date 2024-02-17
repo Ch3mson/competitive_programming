710c5980c7f5e6d580e19c8d737f683698087c0a
Benson Yan <bensonyan778@hotmail.com>
2024-02-17 18:22:46 -0500

    242_valid_anagram.py

---
+++ 242_valid_anagram.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        lib = {}
+
+        for i in nums:
+            if str(i) in lib.keys():
+                return True
+            else:
+                lib[str(i)] = 1
+        
+        return False
+  
