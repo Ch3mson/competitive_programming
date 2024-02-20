93bdf195305e3c921f8be028680b6d1d93b51ea2
Benson Yan <bensonyan778@hotmail.com>
2024-02-19 20:43:25 -0500

    Create 20_valid_paranthesis.py

---
+++ 20_valid_paranthesis.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        dic = {"}": "{", ")": "(", "]": "["}
+        stack = []
+
+        for c in s:
+            if stack and c in dic:
+                if stack[-1] == dic[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        if stack:
+            return False
+        else:
+            return True
