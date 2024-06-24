73c92721db77e065a2c8644b3c72b61024f4ff6c
Benson Yan <bensonyan778@hotmail.com>
2024-06-24 12:32:17 -0400

    Update 118_pascals_triangle.py

--- 118_pascals_triangle.py
+++ 118_pascals_triangle.py
@@ -3,17 +3,16 @@ class Solution:
         if numRows == 0:
             return []
 
-        outputArr = [[1]]  # Initialize with the first row
+        outputArr = [[1]]
 
         for i in range(1, numRows):
-            lastArr = outputArr[-1]  # Get the last row from the output array
-            newArr = [1]  # First element of each row is 1
+            lastArr = outputArr[-1]
+            newArr = [1]
 
             for j in range(1, len(lastArr)):
-                # Append sum of adjacent elements from the last row to the new row
                 newArr.append(lastArr[j-1] + lastArr[j])
 
-            newArr.append(1)  # Last element of each row is 1
-            outputArr.append(newArr)  # Append the newly created row to the output array
+            newArr.append(1)
+            outputArr.append(newArr) 
 
         return outputArr
