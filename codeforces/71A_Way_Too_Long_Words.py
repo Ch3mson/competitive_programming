9c25d85726928dbc179bca60fb459764a217ad4d
Benson Yan <bensonyan778@hotmail.com>
2024-07-04 16:14:18 +0000

    easy

--- codeforces/71A_Way_Too_Long_Words.py
+++ codeforces/71A_Way_Too_Long_Words.py
@@ -1,5 +1,6 @@
 
-def processWord(input: str) -> str:
+# the logic to process the word
+def processWord(input: str) -> str: 
     charArr = list(input)
     if len(charArr) <= 10:
         return input
@@ -8,12 +9,13 @@ def processWord(input: str) -> str:
         return charArr[0] + middle + charArr[-1]
 
 num_lines = int(input())  # number of lines we will process
-word_list = []
+word_list = [] #put each input so we can print at the end
 
-while num_lines > 0:
+
+while num_lines > 0: # input each word
     output = input()
     word_list.append(output)
     num_lines-= 1
 
-for word in word_list:
+for word in word_list: # process and print each word
     print(processWord(word))
