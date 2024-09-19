dc5dfd6719dadd1eb86aaa61582de583147d2a55
Benson Yan <bensonyan778@hotmail.com>
2024-09-18 23:30:11 -0400

    Update 1_Two_sum.py

--- leetcode/1_Two_sum.py
+++ leetcode/1_Two_sum.py
@@ -6,3 +6,17 @@ class Solution:
                 return [dict[target - nums[i]], i]
             else:
                 dict[nums[i]] = i
+
+# second solution with get() and enumerate
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for i, num in enumerate(nums):
+            dic.get(i)
+            compliment = target - num
+            if compliment in dic:
+                return [dic[compliment], i]
+            else:
+                dic[num] = i
+        
