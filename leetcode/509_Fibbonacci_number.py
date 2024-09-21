f8fa25ed64b65877e8bd73fec3bf690b3d2a34b5
Benson Yan <bensonyan778@hotmail.com>
2024-09-21 12:14:05 -0400

    Update 509_Fibbonacci_number.py

--- leetcode/509_Fibbonacci_number.py
+++ leetcode/509_Fibbonacci_number.py
@@ -1,3 +1,4 @@
+# Recursively
 class Solution:
     def fib(self, n: int) -> int:
         
@@ -13,3 +14,14 @@ class Solution:
             return mem[n]
 
         return fib_memo(n, memo)
+
+# With Loop
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        a, b = 0, 1
+
+        for _ in range(2, n+1):
+            a, b = b, a + b
+        return b
