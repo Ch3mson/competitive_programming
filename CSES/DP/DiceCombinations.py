8a7634c7e57f680f27bdf3f071bdfbd663d04c0d
Benson Yan <bensonyan778@hotmail.com>
2025-11-23 21:44:15 -0800

    Implement dice combinations dynamic programming solution

---
+++ CSES/DP/DiceCombinations.py
@@ -0,0 +1,12 @@
+n = int(input())
+MOD = 10**9 + 7
+
+dp = [0]*(n + 1)
+dp[0] = 1
+
+for i in range(1, n + 1):
+  for j in range(1, 7):
+    if j <= i:
+      dp[i] = (dp[i] + dp[i-j]) % MOD
+
+print(dp[n])
